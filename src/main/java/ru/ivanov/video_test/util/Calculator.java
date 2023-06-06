f30c00b8d76cb7d18612ec7e8c00fe19eab971ee
jarvis <dmitrii.zolotenkov@diginetica.com>
2023-06-06 11:30:10 +0400

    Init

---
+++ src/main/java/ru/ivanov/video_test/util/Calculator.java
@@ -0,0 +1,15 @@
+package ru.ivanov.video_test.util;
+
+public class Calculator {
+  public int sum(int a, int b) {
+    return a + b;
+  }
+
+  public boolean evenOrOdd(int number) {
+    return number % 2 == 0;
+  }
+
+  public int divide(int a, int b) {
+    return a / b;
+  }
+}
