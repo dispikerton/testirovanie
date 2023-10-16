a71c6ea40d8535d6a6d25cf24dacb5511d9bb8f3
jarvis <dmitrii.zolotenkov@diginetica.com>
2023-10-16 11:29:39 +0400

    NoArgsConstructor

--- src/main/java/ru/ivanov/video_test/Worker.java
+++ src/main/java/ru/ivanov/video_test/Worker.java
@@ -2,9 +2,11 @@ package ru.ivanov.video_test;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Worker {
   private long id;
   private String name;
