57c78ea5da595a89356bfed2670c3a2f1f6721ab
jarvis <dmitrii.zolotenkov@diginetica.com>
2023-11-10 11:27:49 +0400

    @WebMvcTest

--- src/test/java/ru/ivanov/video_test/WorkerControllerTest.java
+++ src/test/java/ru/ivanov/video_test/WorkerControllerTest.java
@@ -8,34 +8,24 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@ExtendWith(MockitoExtension.class)
+@WebMvcTest(WorkerController.class)
 class WorkerControllerTest {
-  @Mock
+  @MockBean
   private WorkerService workerService;
 
-  @InjectMocks
-  private WorkerController workerController;
-
+  @Autowired
   private MockMvc mockMvc;
+  @Autowired
   private ObjectMapper objectMapper;
-
-  @BeforeEach
-  void setUp() {
-    mockMvc = MockMvcBuilders.standaloneSetup(workerController).build();
-    objectMapper = new ObjectMapper();
-  }
 
   @Test
   void sumOfSalaries() throws Exception {
